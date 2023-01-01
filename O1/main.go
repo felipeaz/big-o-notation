5eef6899a69cf5f826b02a0d068f8051f1ad34ba
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2023-01-01 17:39:47 -0300

    Refactor: renaming array type

--- O1/main.go
+++ O1/main.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-type arrayObject []int
+type integerArray []int
 
 /*The O(1) complexity describes an algorithm where the complexity is constant, that means the input size
 won't impact to the function growth.*/
 func main() {
 	execTime := time.Now()
 
-	arrayOfIntegers := arrayObject{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	arrayOfIntegers := integerArray{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	arrayOfIntegers.getElementByIndex(0)
 
 	fmt.Printf("Executed in %v ns", time.Now().Sub(execTime).Nanoseconds())
@@ -20,6 +20,6 @@ func main() {
 
 /*An easy example and the most used to describe this complexity is gathering the first element of an array,
 we can simply get it using the index.*/
-func (ar arrayObject) getElementByIndex(index int) int {
+func (ar integerArray) getElementByIndex(index int) int {
 	return ar[index]
 }
