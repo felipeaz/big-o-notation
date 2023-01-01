9f5569041e719bca6c8092dff84c32f0f6700b7b
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2023-01-01 17:32:54 -0300

    O(1) Complexity example

---
+++ O1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type arrayObject []int
+
+/*The O(1) complexity describes an algorithm where the complexity is constant, that means the input size
+won't impact to the function growth.*/
+func main() {
+	execTime := time.Now()
+
+	arrayOfIntegers := arrayObject{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	arrayOfIntegers.getElementByIndex(0)
+
+	fmt.Printf("Executed in %v ns", time.Now().Sub(execTime).Nanoseconds())
+}
+
+/*An easy example and the most used to describe this complexity is gathering the first element of an array,
+we can simply get it using the index.*/
+func (ar arrayObject) getElementByIndex(index int) int {
+	return ar[index]
+}
