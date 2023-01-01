72713cc2233a6237b26c3bccac7f04854116a5c8
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2023-01-01 17:41:57 -0300

    Refactor: logging instead of using fmt

--- O1/main.go
+++ O1/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 )
 
@@ -15,11 +15,13 @@ func main() {
 	arrayOfIntegers := integerArray{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	arrayOfIntegers.getElementByIndex(0)
 
-	fmt.Printf("Executed in %v ns", time.Now().Sub(execTime).Nanoseconds())
+	log.Printf("Executed in %v ns", time.Now().Sub(execTime).Nanoseconds())
 }
 
 /*An easy example and the most used to describe this complexity is gathering the first element of an array,
 we can simply get it using the index.*/
 func (ar integerArray) getElementByIndex(index int) int {
-	return ar[index]
+	el := ar[index]
+	log.Printf("Element at index %v is: %v", index, el)
+	return el
 }
